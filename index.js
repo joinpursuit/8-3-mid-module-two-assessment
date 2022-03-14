0ed3ced0d23cd73df0f95696e5156d9937e6504a
Edwin Codrington <edwincodrington@gmail.com>
2022-03-14 11:45:13 -0400

    Final commit: Beautified.

--- index.js
+++ index.js
@@ -205,13 +205,15 @@ function checkMinMetascores(movies, metascore) {
 function getRottenTomatoesScoreByMovie(movies) {
 	if (!movies.length) {
 		throw `Error: movies array is empty!`;
-  }
-  return movies.map((movie) => {
-    let movieTitle = movie.title;
-    let movieRating = movie.ratings.find((rating) => rating.source === "Rotten Tomatoes");
-    movieRating = movieRating.value;
-    return { [movieTitle]: movieRating };
-  })
+	}
+	return movies.map((movie) => {
+		let movieTitle = movie.title;
+		let movieRating = movie.ratings.find(
+			(rating) => rating.source === "Rotten Tomatoes"
+		);
+		movieRating = movieRating.value;
+		return { [movieTitle]: movieRating };
+	});
 }
 
 // Do not change anything below this line.
