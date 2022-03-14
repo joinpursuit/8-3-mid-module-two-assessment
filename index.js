c43d093a8de61ec656a3cb06c297eee38ab0b4dc
arupay <arupay11374@gmail.com>
2022-03-14 10:13:09 -0400

    getAllMovies error condition set

--- index.js
+++ index.js
@@ -30,7 +30,11 @@ const exampleMovies = require("./movies");
       "James and the Giant Peach",
     ];
  */
-function getAllMovieTitles() {}
+function getAllMovieTitles(movies) {
+  if (!movies.length) {
+    throw `There are no movies in ${movies}`;
+  }
+}
 
 /**
  * checkIfAnyMovieHasRating()
